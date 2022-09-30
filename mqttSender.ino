b5b135f5571abc347dc6d1d4054be21191860854
AmanGusainChitkra <113379582+AmanGusainChitkra@users.noreply.github.com>
2022-10-01 02:02:34 +0530

    Create mqttSender.ino

---
+++ mqttSender.ino
@@ -0,0 +1,100 @@
+
+//This code is made by Aman Gusain.
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_ARCH_ESP32)
+  #include <WiFi.h>
+#endif
+
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "broker.hivemq.com";
+int        port     = 1883;
+const char topic[]  = "SIT210/wave";
+
+const long interval = 1000;
+unsigned long previousMillis = 0;
+//Pins of ultrasonic sensors
+const int trig = 4; 
+const int echo = 3; 
+float timet;    //echo time of ultrasonic sensor
+float distanceCM;//distance in CM
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+ 
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+  Serial.print("Message sent: Aman beginning transmission!");
+    mqttClient.beginMessage(topic);
+    mqttClient.print("Aman beginning transmission!");
+    mqttClient.endMessage();
+
+    pinMode(trig, OUTPUT);
+    pinmode(echo, INPUT);
+
+}
+
+void loop() {
+  // call poll() regularly to allow the library to send MQTT keep alives which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
+
+  
+    //ultrasonic
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  
+  timet = pulseIn(echo,HIGH);
+  distanceCM=timet*0.017; //distance
+  
+  if(distanceCM < 10){
+    Serial.println("Wave detected!");
+    mqttClient.beginMessage(topic);
+    mqttClient.print("1");
+    mqttClient.endMessage();
+  }
+}
